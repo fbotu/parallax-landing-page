c240fb3cc04ede64e8b8d14913bb60df81c6c786
fbotu <francisbotu@gmail.com>
2022-01-23 20:31:03 +0000

    sanity deployed

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,15 +1,17 @@
 import Link from 'next/link'
 import groq from 'groq'
 import client from '../../client'
+/* STYLES */
+import styles from '../../styles/post.module.scss'
 
 const Index = ({posts}) => {
     return (
-      <div>
-        <h1>Welcome to a blog!</h1>
+      <div className={styles.pageWrapper}>
+        <h1 className={styles.title}>Welcome to a blog!</h1>
         {posts.length > 0 && posts.map(
           ({ _id, title = '', slug = '', publishedAt = '' }) =>
             slug && (
-              <li key={_id}>
+              <li key={_id} className={styles.postList}>
                 <Link href="/post/[slug]" as={`/post/${slug.current}`}>
                   <a>{title}</a>
                 </Link>{' '}
